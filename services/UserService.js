1f5a3d9879cff9dd8bae17446cd713dc897e69d8
EmanuelG1n0 <emanuelginovski04@gmail.com>
2024-11-03 23:53:44 -0300

    Correciones de user

--- services/UserService.js
+++ services/UserService.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { User, Cart} from "../models/index.js";
 import sequelize from "../connection/connection.js";
+import Role from '../models/Role.js'; 
 
 class UserService {
 
